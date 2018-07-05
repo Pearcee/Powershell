bea01304a25aed2a1e2d261ddb7a5235582b33e7
Steven Pearce <Steven@waverley-housing.co.uk>
2018-07-05 11:08:12 +0100

    add dir

---
+++ Onliners/one-liners.ps1
@@ -0,0 +1,19 @@
+# q how do i get ctrl+j add snippitts?
+<#
+#Set up
+Set local ExecutionPolicy to RemoteSigned
+
+Set-ExecutionPolicy -Scope LocalMachine -ExecutionPolicy RemoteSigned -Force
+Set-ExecutionPolicy Unrestricted
+
+#PowerShell Gallery - as admin
+Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+Update-Module PowerShellGet -Force
+Install-Module Posh-Git -Scope AllUsers
+#>
+
+
+<#
+# powershell 6 - pwsh
+get-computerinfo
+#>
